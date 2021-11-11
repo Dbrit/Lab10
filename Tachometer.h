24ee4aa9e216cee5d322a18b86cf76d045d757f1
Dakota Britton <55359304+Dbrit@users.noreply.github.com>
2021-11-10 18:15:46 -0600

    Added tachometer.h

---
+++ Tachometer.h
@@ -0,0 +1,11 @@
+
+#include <stdint.h>
+#include "../inc/tm4c123gh6pm.h"
+#include "../inc/LaunchPad.h"
+#include "../inc/CortexM.h"
+
+// Initialize tachometer
+void Tachometer_Init(void);
+
+// Get motor RPM
+int getRPM(void);
